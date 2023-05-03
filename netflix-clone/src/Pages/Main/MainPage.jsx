3dbee4ce7d30fc009fed2030b27cc5f9a47504c7
nkovlev <nkovlev4@gmail.com>
2023-05-03 08:50:31 +0300

    main

--- netflix-clone/src/Pages/Main/MainPage.jsx
+++ netflix-clone/src/Pages/Main/MainPage.jsx
@@ -3,22 +3,32 @@ import back from '../../images/hero.jpg';
 import logo from '../../images/logo.png'
 import Header from '../../Components/Header';
 import { Link } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai'
+import { useState } from 'react';
+
 
 const MainPage = () => {
+
+
+  // const [selectedItem, setSelectedItem] = useState('Home');
+  
   return (
-    <>
-      <div className='h-screen bg-cover bg-center' style={{ backgroundImage: `url(${back})` }}>
-        <div className="bg-black w-full h-full lg:bg-opacity-50 ">
+    <div className='bg-zinc-900 h-full overflow-x-hidden'>
           <Header>
-                <div className="flex justify-between h-28 items-center">
-                    <Link to={'/'}><img src={logo} alt="logo" className='h-11 pl-10' /></Link>
-                    <Link to={'/signin'} className='font-semibold text-xl 0 pr-10 hover:underline'>Sign In</Link>
+                <div className="w-full fixed z-40">
+                  <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
+                  <img className='h4 lg:h-7' src={logo} alt="logo" />
+                  <div className="flex-row ml-8 gap-7 hidden lg:flex">
+                    <p className='text-white cursor-pointer hover:text-fray-300 transition font-light'>Home</p>
+                    <p className='text-white cursor-pointer hover:text-fray-300 transition font-light'>TV Shows</p>
+                    <p className='text-white cursor-pointer hover:text-fray-300 transition font-light'>Movies</p>
+                    <p className='text-white cursor-pointer hover:text-fray-300 transition font-light'>New & Popular</p>
+                    <p className='text-white cursor-pointer hover:text-fray-300 transition font-light'>My List</p>
+                  </div>
+                  </div>
                 </div>
           </Header>
-          <h1 className='text-white text-3xl font-bold'>Main Page</h1>
-        </div>
-      </div>
-    </>
+    </div>
     );
 }
 
