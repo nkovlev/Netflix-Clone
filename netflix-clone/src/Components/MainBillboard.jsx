4dce13b5df04996258e4c7b0b6e383b4eaa3f6cf
nick <nkovlev4@gmail.com>
2023-05-17 17:50:13 +0300

    bilboard add

--- netflix-clone/src/Components/MainBillboard.jsx
+++ netflix-clone/src/Components/MainBillboard.jsx
@@ -1,76 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import ReactPlayer from 'react-player';
+import movies from '../movies.json';
 
 const MainBillboard = () => {
   const [randomMovie, setRandomMovie] = useState(null);
 
   const fetchRandomMovie = async () => {
-    const apiKey = '07a58923108a39dacfeebe9fa77e366c';
-    const endpoint = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
-    try {
-      const response = await axios.get(endpoint);
-      const movies = response.data.results;
-      const randomIndex = Math.floor(Math.random() * movies.length);
-      const randomMovie = movies[randomIndex];
-      return randomMovie;
-    } catch (error) {
-      console.error('Error fetching random movie:', error);
-    }
-  };
-
-  const fetchVideoInfo = async (movieId) => {
-    const apiKey = '07a58923108a39dacfeebe9fa77e366c';
-    const endpoint = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`;
-    try {
-      const response = await axios.get(endpoint);
-      const videos = response.data.results;
-      if (videos.length > 0) {
-        const videoInfo = videos[0];
-        return videoInfo;
-      }
-    } catch (error) {
-      console.error('Error fetching video info:', error);
-    }
+    const randomIndex = Math.floor(Math.random() * movies.length);
+    const randomMovie = movies[randomIndex];
+    return randomMovie;
   };
 
   useEffect(() => {
     const getRandomMovie = async () => {
       const movie = await fetchRandomMovie();
-      const videoInfo = await fetchVideoInfo(movie.id);
-      movie.videoInfo = videoInfo;
       setRandomMovie(movie);
     };
     getRandomMovie();
   }, []);
 
   return (
-    <div className='relative w-full'>
-      {randomMovie && randomMovie.videoInfo && (
-        <div>
-          <ReactPlayer
-            url={`https://www.youtube.com/watch?v=${randomMovie.videoInfo.key}`}
-            playing={true}
-            muted={true}
-            controls={false}
-            loop={true}
-            width='100%'
-            height='100%'
-            style={{ objectFit: 'cover' }}
-            config={{
-              youtube: {
-                playerVars: {
-                  modestbranding: 1,
-                  controls: 0,
-                },
-              },
-            }}
-          />
-        </div>
-      )}
-    </div>
-  );
-  
+<div className="relative w-screen h-screen z-1 overflow-hidden">
+  {randomMovie && (
+    <video
+      autoPlay
+      muted
+      loop
+      className="w-full h-full object-cover absolute top-0 left-0"
+      src={randomMovie.videoUrl}
+    ></video>
+  )}
+</div>
+);
 };
 
 export default MainBillboard;
--- netflix-clone/src/Pages/Main/MainPage.jsx
+++ netflix-clone/src/Pages/Main/MainPage.jsx
@@ -31,7 +31,7 @@ const MainPage = () => {
     <>
           <Header>
                 <div className="w-full fixed z-40">
-                  <div className="px-4 md:px-16 py-4 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90">
+                  <div className="px-4 md:px-16 py-4 flex flex-row items-center transition duration-500 bg-transparent bg-opacity-90">
                   <img className='h-4 lg:h-6' src={logo} alt="logo" />
                   <div className="flex-row ml-7 gap-7 hidden lg:flex">
                     <p className='text-white cursor-pointer hover:text-fray-300 transition font-light'>Home</p>
