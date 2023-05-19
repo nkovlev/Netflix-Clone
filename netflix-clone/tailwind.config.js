007dafbd95d30ef182cd2a26a2b8262b36297739
nick <nkovlev4@gmail.com>
2023-05-19 18:07:15 +0300

    list

--- netflix-clone/tailwind.config.js
+++ netflix-clone/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+          'fade-in': {
+             '0%': { opacity: '0', transform: 'scale(0.5)'},
+             '100%': { opacity: '1', transform: 'scale(1.3) ' },
+             },
+          },
+    animation: {
+      'fade-in': 'fade-in 0.3s ease-out',
+    },
+  },
   },
   plugins: [require('tailwind-scrollbar-hide')],
 }
