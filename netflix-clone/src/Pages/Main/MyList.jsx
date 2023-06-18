dd96aa3def570f44a7481f6061c25cc408560608
nkovlev <nkovlev4@gmail.com>
2023-06-18 18:44:17 +0300

    my list in proc

--- netflix-clone/src/Pages/Main/MyList.jsx
+++ netflix-clone/src/Pages/Main/MyList.jsx
@@ -3,18 +3,25 @@ import React from 'react'
 const MyList = ({ myList }) => {
   return (
     <div className='bg-black w-screen h-screen'>
-        <h2>My List</h2>
-      {myList.map((movie) => (
-        <div className="group col-span relative transition-opacity w-60 mt-10 mb-40 flex">
-        <div className="overflow-hidden rounded-md shadow-xl group-hover:shadow-2xl">
-        <img
-          className="h-full w-full object-cover cursor-pointer duration transition transform scale-100 group-hover:scale-105"
-          src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-          alt={movie.title}
-        />
+        <div className="pt-20 pl-16">
+        <h2 className='text-white text-3xl mb-24'>My List</h2>
+        <div className="flex overflow-x-auto">
+        {myList.map((movie) => (
+          <div
+            key={movie.id}
+            className="group relative transition-opacity w-60 mr-4 flex-shrink-0"
+          >
+            <div className="overflow-hidden rounded-md shadow-xl group-hover:shadow-2xl">
+              <img
+                className="h-full w-full object-cover cursor-pointer duration transition transform scale-100 group-hover:scale-105"
+                src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                alt={movie.title}
+              />
+            </div>
+          </div>
+        ))}
       </div>
       </div>
-      ))}
     </div>
   )
 }
