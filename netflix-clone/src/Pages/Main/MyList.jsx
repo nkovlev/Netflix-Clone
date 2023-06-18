c27ff2928b491d8e89bcbc4cd7595a8cc33346ec
nkovlev <nkovlev4@gmail.com>
2023-06-18 18:27:33 +0300

    my list in proc

--- netflix-clone/src/Pages/Main/MyList.jsx
+++ netflix-clone/src/Pages/Main/MyList.jsx
@@ -5,9 +5,15 @@ const MyList = ({ myList }) => {
     <div className='bg-black w-screen h-screen'>
         <h2>My List</h2>
       {myList.map((movie) => (
-        <div key={movie.id}>
-          <h3 className='text-white'>{movie.title}</h3>
-        </div>
+        <div className="group col-span relative transition-opacity w-60 mt-10 mb-40 flex">
+        <div className="overflow-hidden rounded-md shadow-xl group-hover:shadow-2xl">
+        <img
+          className="h-full w-full object-cover cursor-pointer duration transition transform scale-100 group-hover:scale-105"
+          src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+          alt={movie.title}
+        />
+      </div>
+      </div>
       ))}
     </div>
   )
